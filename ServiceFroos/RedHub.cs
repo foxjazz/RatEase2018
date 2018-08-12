cd472124df824782bcf027f087659d6cc2ca3e25
mordagel <mordagel@outlook.com>
2018-08-12 16:00:01 -0600

    added asp.net signalR service

---
+++ ServiceFroos/RedHub.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using System.Drawing;
+using System.IO;
+
+namespace ServiceFroos
+{
+    public class RedHub : Hub
+    {
+
+        public void SendBmp(string user, BitMapMessage message)
+        {
+            //todo transform image to InfoMessage
+            var myUniqueFileName = $@"{Guid.NewGuid()}";
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(Startup.path + "\\" + myUniqueFileName + ".nam"))
+            {
+                file.WriteLine(user);
+            }
+            message.bitmap.Save(Startup.path + "\\" + myUniqueFileName + ".bmp");
+
+            //write two files user and bmp for further processing.
+
+            //await Clients.Caller.SendAsync("Acknowlege", user, message);
+        }
+
+        public Task SendMessageToCaller(InfoMessage message)
+        {
+            return Clients.Caller.SendAsync("ReceiveMessage", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "RatEase Users");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "RatEase Users");
+            await base.OnDisconnectedAsync(exception);
+        }
+        public Task SendMessageToGroups(string message)
+        {
+            List<string> groups = new List<string>() { "RatEase Users" };
+            return Clients.Groups(groups).SendAsync("ReceiveMessage", message);
+        }
+
+
+    }
+
+    public class BitMapMessage
+    {
+        public Bitmap bitmap { get; set; }
+        public string eveSystem { get; set; }
+        
+
+    }
+
+    public class InfoMessage
+    {
+        public string[] redPlayers { get; set; }
+        public string eveSystem { get; set; }
+    }
+}
